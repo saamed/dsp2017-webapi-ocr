17531b89c0b906785b5ba2fecb6b144d461dbbd3
bclapa <bclapa@gmail.com>
2017-03-08 19:22:03 +0100

    binaryzacja obrazów + niezbędne biblioteki wymagane jest skopiowanie do katalogu Libs w głównym katalogu projektu, plików cvextern.dll, msvcp140.dll, opencv_ffmpeg310.dll, vcruntime140.dll, Emgu.CV.World.dll z katalogu instalacyjnego Emgu.CV

---
+++ DSP.ImageConverter/ImageConverterHelper.cs
@@ -0,0 +1,21 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace DSP.ImageConverter
+{
+  public static class ImageConverterHelper
+  {
+    public static void ConvertToBinary(string filepath, string destination, double threshold = 150)
+    {
+      var img = CvInvoke.Imread(filepath);
+      var dst = new Mat();
+      CvInvoke.CvtColor(img, dst, ColorConversion.Bgr2Gray); // konwersja do szarości
+
+      // binaryzacja obrazu
+      var image = new Image<Gray, byte>(dst.Bitmap).ThresholdBinary(new Gray(threshold), new Gray(255));
+        
+      image.Save(destination);
+    }
+  }
+}
---
+++ DSP.ImageConverter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace DSP.ImageConverter
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (args.Length != 1)
+        return;
+
+      var path = args[0];
+      
+
+      var filePaths = Directory.GetFiles(path);
+
+      foreach (var filePath in filePaths)
+      {
+        var fileName = Path.GetFileNameWithoutExtension(filePath);
+        if (Regex.IsMatch(fileName, "._binarized"))
+          continue;
+        var newFilePath = Path.Combine(path, fileName + "_binarized" + Path.GetExtension(filePath));
+
+        ImageConverterHelper.ConvertToBinary(filePath,newFilePath, 130);
+      }
+    }
+  }
+}
